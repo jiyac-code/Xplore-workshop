1ca677c4b9b9aeb44c676066fa9f7c23c9eabd87
prlokhandeb25-cyber <prlokhande_b25@ce.vjti.ac.in>
2026-03-12 22:07:18 +0530

    baiscs done

--- prlokhandeb25-cyber_solutions/test_playground/basics/boss.py
+++ prlokhandeb25-cyber_solutions/test_playground/basics/boss.py
@@ -1,49 +1,43 @@
 choice = 'y'
 
-while choice =='y' : # make 'Y' valid too
+while choice.lower() =='y' : # make 'Y' valid too
     try:
-        # typecast the below 2 to a list
+        expr=input("Enter arithmetic Expression: ")
+        result=eval(expr)
+        print("output: ",result)
         
-        numbers =input("Enter the input numbers separated by spaces: ")
-        operators = input("Enter operators between them: ")
-
-        # check length matching
-
-        if len(numbers) != len(operators): # this seems odd... u might say it's ... off by one
-            print("What u doin fam ? ") # replace wiht better message :)
-            continue
-        
-        flag = False # huh this seems inverted
-        for i in range(len(numbers)): # indexing range fix
-            a, b, op = numbers[i-1], numbers[i], operators[i]
+        #numbers =list(map(int,input("Enter the input numbers separated by spaces: ").split()))
+        #operators = input("Enter operators between them: ").split()
+        #if len(operators) != len(numbers)-1: 
+            #print("INVALID EXPR ") 
+            #continue
+        #flag = True 
+        #result=numbers[0]
+        #for i in range(len(operators)): 
+            #a, b, op = result, numbers[i+1], operators[i]
             # correct the ops
-            match op:
-                case '+':
-                    c = a + b
-                case '-':
-                    c = a * b
-                case '*':
-                    c = a / b
-                case '/':
-                    c = a % b
-                case '%':
-                    c = a - b
-                case '//':
-                    c = a ** b
-                case '**':
-                    c = a // b
-                case _:
-                    flag = True
-            if not flag:
-                print("Invalid ops vro")
-                break
-
-            numbers[i-1] = c
-        if not flag:
-            continue
-        print(f"Output: numbers[-1]")
-    except Exception:
-        print(f"Exception: ...") # print exception
+            #if op == '+':
+               #result = a + b
+            #elif op == '-':
+                #result = a - b
+            #elif op == '*':
+                #result = a * b
+            #elif op == '/':
+                #result = a / b
+            #elif op == '%':
+                #result = a % b
+            #elif op == '//':
+                #result = a // b
+            #elif op == '**':
+                #result = a ** b
+            #else:
+                #flag = False
+                #print("Invalid operator detected!")
+                #break
+        #if flag:
+            #print(f"Output: {result}")
+    except Exception as e :
+        print(f"Exception:{e}") # print exception
     finally:
         choice = input("Do you want to continue? [y/n] : ") # always ask before ending
 
