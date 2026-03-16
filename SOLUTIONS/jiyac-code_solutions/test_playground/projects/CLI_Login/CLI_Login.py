763b3d4b5d6a0b7f6084e599cdc56a70e2a86376
jiyac-code <jiyac-code@users.noreply.github.com>
2026-03-16 11:26:07 +0000

    deploy: 07a62da46a644f209ad05ddf37d29736e12ec1bc

--- SOLUTIONS/jiyac-code_solutions/test_playground/projects/CLI_Login/CLI_Login.py
+++ SOLUTIONS/jiyac-code_solutions/test_playground/projects/CLI_Login/CLI_Login.py
@@ -1,17 +1,34 @@
 
 # Created a CLI login/signup system 
 
+import os
+import time
+import hashlib
+
+# Created a function to clear screen
+def clear_screen():
+    time.sleep(1.5)
+    os.system('cls' if os.name=='nt' else clear)
+
+
+# Created a function to hash password
+def hash(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
 # Created a function to register the user
 def signup():
     check=False
     while not check:
+        clear_screen()
         print("\n---Sign Up---")
         name=input("Enter your name: ")
         password=input("Enter your password: ")
         confirm=input("Confirm password: ")
         if(password==confirm):                          # If password does match confirm password, then the user is registered
+            hashed_pass=hash(password)                  # Password is hashed and then stored
             with open("reg_users.txt","a") as f:
-                f.write(name+","+password+"\n")
+                f.write(name+","+hashed_pass+"\n")
             print("You have successfully registered.\n")
             check=True
         else:                                           
@@ -20,15 +37,18 @@ def signup():
 
 # Created a function to login a registered user
 def login():
+    clear_screen()
     print("\n---Login---")
     name=input("Enter your name: ")
     password=input("Enter your password: ")
+    hashed_pass=hash(password)                          # Password entered by user is hashed and compared with stored password
 
     with open("reg_users.txt", "r") as f:
         for line in f:
             stored_name, stored_pass=line.strip().split(",")
-            if(stored_name==name and stored_pass==password):
+            if(stored_name==name and stored_pass==hashed_pass):
                 print("You have successfully logged in!\n")
+                clear_screen()
                 print("Welcome",name)
                 return True
   
@@ -41,6 +61,7 @@ def main():
     is_logged_in=False
     while choice !=3:
         if not is_logged_in:                                # Home Screen is displayed if user is not logged in
+            clear_screen()
             print("\n---Home---")
             print(" 1. Login","\n","2. Sign Up","\n","3. Quit")
             choice=int(input("Enter your choice: "))
